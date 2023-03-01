11dd7b47e60010a4f4ae1e21448a8f43eceee75d
Angel Cepeda <a338832@uach.mx>
2023-03-01 14:50:34 -0700

    change to model

--- prediction.py
+++ prediction.py
@@ -1,21 +1,22 @@
 import joblib
 
 def predict(data, model):
+    
     if model == 'Linear Regression':
-        modelpkl = joblib.load('linear_regression_model.pkl')
+        model = joblib.load('linear_regression_model.pkl')
     
     elif model == 'Decision Tree Regression':
-        modelpkl =joblib.load('tree_regression_model.pkl')
+        model =joblib.load('tree_regression_model.pkl')
     
     elif model == 'Random Forest Regression':
-        modelpkl == joblib.load('random_forest_model.pkl')
+        model == joblib.load('random_forest_model.pkl')
     
     elif model == 'Support Vector Regression':
-        modelpkl == joblib.load('svr_regression_model.pkl')
+        model == joblib.load('svr_regression_model.pkl')
 
     pipeline =joblib.load("full_pipeline.pkl")
     data = pipeline.transform(data)
     
-    return modelpkl.predict(data)
+    return model.predict(data)
 
 
